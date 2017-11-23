e79acf85781454d5d57c949ea693b62f11995fb7
shuishui <864805002@qq.com>
2017-11-23 15:45:39 +0800

    Update Engine.java

--- demo/src/main/java/cn/bingoogolapple/refreshlayout/demo/engine/Engine.java
+++ demo/src/main/java/cn/bingoogolapple/refreshlayout/demo/engine/Engine.java
@@ -6,7 +6,10 @@ import cn.bingoogolapple.refreshlayout.demo.model.BannerModel;
 import cn.bingoogolapple.refreshlayout.demo.model.RefreshModel;
 import cn.bingoogolapple.refreshlayout.demo.model.StaggeredModel;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
